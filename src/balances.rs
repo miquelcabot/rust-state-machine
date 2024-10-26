15b529d9f1d8ddedc4faf54f234450d8b1ad08be
miquelcabot <miquel.cabot@gmail.com>
2024-10-26 11:13:57 +0200

    add set_balance() and balance() functions

--- src/balances.rs
+++ src/balances.rs
@@ -1,11 +1,23 @@
 use std::collections::BTreeMap;
 
 pub struct Pallet {
-    balances: BTreeMap<String, u32>,
+    balances: BTreeMap<String, u128>,
 }
 
 impl Pallet {
+    /// Create a new instance of the balances module.
     pub fn new() -> Self {
         Pallet { balances: BTreeMap::new() }
+    }
+
+    /// Set the balance of an account `who` to some `amount`.
+    pub fn set_balance(&mut self, who: &String, amount: u128) {
+        self.balances.insert(who.clone(), amount);
+    }
+
+    /// Get the balance of an account `who`.
+    /// If the account has no stored balance, we return zero.
+    pub fn balance(&self, who: &String) -> u128 {
+        *self.balances.get(who).unwrap_or(&0)
     }
 }
